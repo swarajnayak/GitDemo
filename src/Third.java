7c8a0391fca1aa1e9a6b5dbcf189ceac23f891fc
swarajnayak <swarajnayak015@gmail.com>
2020-09-13 15:41:07 +0530

    Newly made changes to Third in Eclipse

--- src/Third.java
+++ src/Third.java
@@ -1,4 +1,10 @@
 
 public class Third {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("New method added to be pushed into Github");
+	}
+
 }
